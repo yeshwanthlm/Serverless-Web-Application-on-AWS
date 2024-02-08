00cdda38e3e44f1fbc7ddbdbb46cbfd171f3aba4
FrednerFrederic <159076483+FrednerFrederic@users.noreply.github.com>
2024-02-08 11:53:12 -0500

    patch1

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ form.addEventListener('submit', (event) => {
 const counter = document.querySelector(".counter-number");
 async function updateCounter() {
     let response = await fetch(
-        "https://o73g5ptpujgtclinhzhhneplje0jogoh.lambda-url.us-east-1.on.aws/"
+        "https://2vjyddunqymu2by4zuzoqnlode0tehil.lambda-url.us-east-1.on.aws/"
     );
     let data = await response.json();
     counter.innerHTML = `Views: ${data}`;
